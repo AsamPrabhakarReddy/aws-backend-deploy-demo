2152207c983ba07143f6e72231989a075971c810
ASAM PRABHAKAR REDDY <asamprabhakarbala@gmail.com>
2024-09-06 12:01:45 +0530

    first chnage

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} successfully`);
 });
